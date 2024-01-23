6d25bc3cbe59848479f69893a03863881a3c505a
SIGMazer <mazinasd7@gmail.com>
2024-01-23 06:46:44 +0200

    Update collision function

--- src/main.c
+++ src/main.c
@@ -22,14 +22,14 @@ void bulletRender(){
     }
 }
 
-bool checkCollision(GameObject *obj1, GameObject *obj2){
-    if(obj1->pos.x < obj2->pos.x + obj2->size.x &&
-            obj1->pos.x + obj1->size.x > obj2->pos.x &&
-            obj1->pos.y < obj2->pos.y + obj2->size.y &&
-            obj1->pos.y + obj1->size.y > obj2->pos.y){
-        return true;
+bool checkCollision(GameObject *obj1, GameObject *obj2) {
+    if(obj1->pos.x >= obj2->pos.x + obj2->size.x ||
+       obj1->pos.x + obj1->size.x <= obj2->pos.x ||
+       obj1->pos.y >= obj2->pos.y + obj2->size.y ||
+       obj1->pos.y + obj1->size.y <= obj2->pos.y) {
+        return false;
     }
-    return false;
+    return true;
 }
 
 void gameOver(){
